b8a2718faf40ef7b414e20166185237409f59488
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2018-05-02 20:28:15 +0000

    Jonny cree una nueva rama y meta los estilos en esa, yo le hago merge luego

---
+++ src/components/Todayslesson.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Todayslesson = () => (
+  <div>
+    <h1>Today's lesson will be here!</h1>
+  </div>
+)
+
+export default Todayslesson
---
+++ src/components/Units.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Units = () => (
+  <div>
+    <h1>Units will be here!</h1>
+  </div>
+)
+
+export default Units
