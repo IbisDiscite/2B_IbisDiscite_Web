0393858accee9e7e31eee213f6ffd12f3417969b
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2018-05-02 21:27:12 +0000

    primeros pasos a      graphiql

--- src/components/Units.js
+++ src/components/Units.js
@@ -1,9 +1,51 @@
 import React from 'react'
 
+import GlRequest from './graphQLUtils';
+
+const request = `query{
+    allUnits{
+      id
+      nombre
+    }
+}`;
 const Units = () => (
   <div>
     <h1>Units will be here!</h1>
   </div>
 )
 
-export default Units
+//export default Units
+///*
+export default class UnitResults extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    GlRequest(
+      request ,
+      (data) => {
+        this.setState({
+          dataSource: data.allUnits,
+          isLoading: false,
+        })
+      },
+      (status, data) => {
+
+      }
+    );
+  }
+  
+  render() {
+    if(this.state.isLoading){
+      return(
+        <h1>no acá!</h1>
+      )
+    }
+    return(
+        Units()
+      )
+  }
+}
+//*/
