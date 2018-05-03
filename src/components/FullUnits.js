400ba0eaf5ed954f2900cf8a96bcab82a4950160
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2018-05-03 07:13:57 +0000

    añadidos mensajes de excepciones

--- src/components/FullUnits.js
+++ src/components/FullUnits.js
@@ -2,18 +2,24 @@ import React from 'react'
 import ApiUnits from '../apiUnits'
 import { Link } from 'react-router-dom'
 
-const FullUnits = () => (
-  <div>
-    <ul>
-      {
-        ApiUnits.all().map(p => (
-          <li key={p.id}>
-            <Link to={`/units/${p.id}`}>{p.nombre}</Link>
-          </li>
-        ))
-      }
-    </ul>
-  </div>
-)
+const FullUnits = () => {
+  const units = ApiUnits.all();
+  if(units.length===0){
+    return <div><h2>No units available at the moment </h2></div>
+  }
+  return(
+    <div>
+      <ul>
+        {
+          units.map(p => (
+            <li key={p.id}>
+              <Link to={`/units/${p.id}`}>{p.nombre}</Link>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
 
 export default FullUnits
