0b76343896b3db456f8c376043da8eee34adbbbf
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2018-05-03 06:55:02 +0000

    eliminadas rutas inservibles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,6 @@ import './Header.css';
 const Header = () => (
 <div class="topnav">
   <a class="active" href='/'>Home</a>
-  <a href='/roster'>Roster</a>
-  <a href='/schedule'>Schedule</a>
   <a href='/units'>Unit</a>
   <a href='/todayslesson'>Today's Lesson</a>
 </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+
 import Home from './Home'
-import Roster from './Roster'
-import Schedule from './Schedule'
 import Units from './Units'
 import Todayslesson from './Todayslesson'
 
@@ -10,8 +9,6 @@ const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Home}/>
-      <Route path='/roster' component={Roster}/>
-      <Route path='/schedule' component={Schedule}/>
       <Route path='/units' component={Units}/>
       <Route path='/Todayslesson' component={Todayslesson}/>
 
