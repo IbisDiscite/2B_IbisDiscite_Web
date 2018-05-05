6679a13d6dab39cdb157b67b10148845e9369e13
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2018-05-05 05:13:44 +0000

    un poco de orden

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Home from './Home'
+import Home from '../pages/Home'
 import Units from '../pages/Units'
 import Todayslesson from '../pages/Todayslesson'
 
