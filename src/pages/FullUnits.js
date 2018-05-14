a9d87d4347f36557eb8722c24d4ba4e3e93afb03
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2018-05-14 19:35:54 +0000

    units graphql

--- src/pages/FullUnits.js
+++ src/pages/FullUnits.js
@@ -52,29 +52,3 @@ class FullUnits extends Component{
 }
 
 export default FullUnits
-
-
-/*
-const FullUnits = () => {
-  const units = ApiUnits.all();
-  if(units.length===0){
-    return <div><h2>No units available at the moment </h2></div>
-  }
-  return(
-    <div>
-      <ul>
-        {
-          units.map(p => (
-            <li key={p.id}>
-              <Link to={`/units/${p.id}`}>{p.nombre}</Link>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
-  )
-}
-
-export default FullUnits
-
-*/
