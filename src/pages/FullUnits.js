e3c4ade116fc4ec8e56bc920e0c4c3a7f3461bf7
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2018-05-14 19:36:50 +0000

    units graphql

--- src/pages/FullUnits.js
+++ src/pages/FullUnits.js
@@ -2,7 +2,6 @@ import React, { Component }  from 'react'
 import { Link } from 'react-router-dom'
 import GraphQLRequest from '../graphQLUtils';
 
-import ApiUnits from '../apis/apiUnits'
 
 class FullUnits extends Component{
   constructor(props){
