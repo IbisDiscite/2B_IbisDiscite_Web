ac58981a58bf628515ac2e104a5c35eeba4dfe95
Pedro Berrio <pcberriop@unal.edu.co>
2018-05-21 00:42:27 -0500

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
     <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div className="col-md-5 p-lg-5 mx-auto my-5">
             <h1 className="display-4 font-weight-normal">Ibis Discite</h1>
-            <h3 className="display-4 font-weight-normal">Have you ever wonder why you don't understand English ?</h3>
+            <h5 className="display-4 font-weight-normal">Have you ever wonder why you don't understand English ?</h5>
             <p className="lead font-weight-normal">There might be a simple reason, Ibis Discite it's an application that will teach you the proper grammar for different subjects and situations, so you always know what, and how to use it correctly.</p>
             {/*<a className="btn btn-outline-secondary" href="#">Coming soon</a>*/}
 
